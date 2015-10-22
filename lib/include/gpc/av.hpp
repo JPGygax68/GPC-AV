1927be89e5fb572207fe24b32a98ca3fef0090b3
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-10-22 16:37:16 +0200

    Started working on a "Source" class

--- lib/include/gpc/av.hpp
+++ lib/include/gpc/av.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <string>
+
 namespace gpc {
     
     namespace av {
         
+        class Source {
+        public:
+
+            /* Open a stream by its URL.
+             */
+            static auto open(const std::string &url) -> Source;
+
+        };
+
     } // ns av
     
 } // ns gpc
